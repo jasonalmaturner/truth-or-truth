2d75621b13561957cf71316f12494d8c67e10ace
jasonalmaturner <jasonalmaturner@gmail.com>
2014-10-06 17:32:08 -0600

    Working on structure. No styling. Setting up routing

---
+++ app/app.js
@@ -0,0 +1,29 @@
+var app = angular.module('truth', ['ngRoute']);
+
+app.config(['$routeProvider', function($routeProvider){
+
+// $httpProvider.interceptors.push('httpRequestInterceptor');
+
+$routeProvider
+	.when('/home', {
+		templateURL: 'app/js/home/home.html',
+		controller: 'app/js/home/homeCtrl.js'
+	})
+	.when('/create', {
+		templateURL: 'app/js/create/create.html',
+		controller: 'app/js/create/createCtrl.js'
+	})
+	.when('/join', {
+		templateURL: 'app/js/join/join.html',
+		controller: 'ap/js/join/joinCtrl.js'
+	})
+	.when('/hat:gameID', {
+		templateURL: 'app/js/hat/hat.html',
+		controller: 'app/js/hat/hatCtrl.js'
+	})
+	.when('/player:gameID', {
+		templateURL: 'app/js/player/player.html',
+		controller: 'app/js/player/payerCtrl.js'
+	})
+
+}]);
---
+++ app/mainCtrl.js
@@ -0,0 +1,7 @@
+var app = angular.module('truth');
+
+app.controller('mainCtrl', function($scope){
+
+	$scope.test = 'foo';
+
+})
